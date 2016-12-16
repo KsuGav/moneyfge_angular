92aebbeedeab98d80bfce72820e9b27e61aee85d
Ksu <Gaydamanchukksena@gmail.com>
2016-12-16 15:00:57 +0200

    install plugin phone

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -36,7 +36,8 @@ export class LoginComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngAfterViewInit() {
-
+    this.setupTelMask();
+    this.showHidePassword();
   }
 
   ngOnDestroy() {
@@ -70,6 +71,71 @@ export class LoginComponent implements OnInit, OnDestroy, AfterViewInit {
         }
       )
     ;
+  }
+
+  setupTelMask() {
+    let go = 0;
+    $('.phone-input-ua').intlTelInput({
+      utilsScript: "assets/js/intlTelInput/utils.js?5",
+      initialCountry: "auto",
+      defaultCountry: 'auto',
+      customPlaceholder: function (selectedCountryPlaceholder, selectedCountryData) {
+        return "e.g. " + selectedCountryPlaceholder;
+      },
+      geoIpLookup: function (callback) {
+        $.get('http://ipinfo.io', function () {
+        }, "jsonp").always(function (resp) {
+          var countryCode = (resp && resp.country) ? resp.country : "";
+          go = 1;
+
+          callback(countryCode);
+        });
+      }
+    });
+    $('.phone-input-ua').focus();
+    $(".country").click(function () {
+      this_country();
+    });
+    let interval1 = setInterval(function () {
+      this_country();
+    }, 100);
+
+    function this_country() {
+      if (go == 1) {
+        var countryData = $(".phone-input-ua").intlTelInput("getSelectedCountryData");
+        if (typeof(countryData.dialCode) != 'string') {
+          countryData.dialCode = '';
+        }
+        $('.phone-input-ua').val('+' + countryData.dialCode);
+
+        go = 0;
+        return countryData;
+      }
+    }
+  }
+
+  showHidePassword(){
+    $('input[type="password"]').each(function (index, input) {
+      var $input = $(input);
+      $(".input-group .fa-eye").click(function () {
+        var change = "";
+        if ($(this).hasClass("active")) {
+          $(this).removeClass("active");
+          change = "password";
+        } else {
+          $(this).addClass("active");
+          change = "text";
+        }
+        var rep = $("<input type='" + change + "' />")
+          .attr("id", $input.attr("id"))
+          .attr("name", $input.attr("name"))
+          .attr('class', $input.attr('class'))
+          .val($input.val())
+          .insertBefore($input);
+        $input.remove();
+        $input = rep;
+      }).insertAfter($input);
+    });
   }
 
 }
