2fa507e589ef2c621854ebd4f96fc63e68abde3c
Ksu <Gaydamanchukksena@gmail.com>
2016-12-26 15:37:39 +0200

    added provider.ts for i18n

---
+++ src/app/i18n-provider.ts
@@ -0,0 +1,49 @@
+import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID} from '@angular/core';
+
+export function getTranslationProviders(): Object[]{
+  //Get the locale from browser
+  let locale = detectLocale(['ru','ua'], 'en');
+
+  //Load messages by webpack
+  let translations = {
+    'ru' : require('.locale/messages.ru.xlf'),
+    'ua' : require('.locale/messages.ua.xlf')
+  };
+
+  //No locale, no messages, or en: no translation providers
+  if((!locale || locale === 'en') && !translations[locale]){
+    return [];
+  }
+  else {
+    return[
+      {provide: TRANSLATIONS, useValue: translations[locale]},
+      {provide: TRANSLATIONS_FORMAT, useValue: 'xlf'},
+      {provide: LOCALE_ID, useValue: locale}
+    ];
+  }
+}
+
+//Detected locale from browser
+//Pick one from list or return base.
+
+//@param available
+//@param base
+//@return {string}
+
+function detectLocale( available: string[], base: string){
+  let w: any = window;
+  let language = w.navigator.userLanguage || w.navigator.language;
+  if(language.indexOf('-') !== -1){
+    language = language.split('-')[0];
+  }
+  if(language.indexOf('_') !== -1){
+    language = language.split('_')[0];
+  }
+  if(language && available.indexOf(language) !== -1){
+    return language;
+  }
+  else{
+    return base;
+  }
+}
+
