df09e857a2521178bf74818ea106b1afb417f995
Ksu <Gaydamanchukksena@gmail.com>
2016-12-30 14:54:22 +0200

    add separate modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,33 +30,33 @@ import { SiteRecoveryComponent } from './site/recovery';
 import { SiteRegisterComponent } from './site/register';
 import { SiteConfirmComponent } from './site/confirm';
 import { SitePasswordComponent } from './site/password';
-import { CabinetComponent } from './cabinet';
-import { CabinetHeaderComponent } from './cabinet/header';
-import { CabinetFooterComponent } from './cabinet/footer';
-import { CabinetMainComponent } from './cabinet/main';
-import { CabinetPaymentComponent } from './cabinet/payment';
-import { CabinetTransferComponent } from './cabinet/transfer';
-import { TransferDefaultComponent } from './cabinet/transfer/default';
-import { TransferAccountComponent } from './cabinet/transfer/account';
-import { CabinetFillComponent } from './cabinet/fill';
-import { CabinetScoreComponent } from './cabinet/score';
-import { ScoreIndexComponent } from './cabinet/score/default';
-import { ScoreCreateComponent } from './cabinet/score/create';
-import { CabinetOutmoneyComponent } from './cabinet/outmoney';
-import { OutmoneyDefaultComponent } from './cabinet/outmoney/default';
-import { OutmoneyListComponent } from './cabinet/outmoney/list';
-import { CabinetSettingsComponent } from './cabinet/settings';
-import { SettingsProfileComponent } from './cabinet/settings/profile';
-import { PhoneFormComponent } from './cabinet/settings/profile/phone-form';
-import { EmailFormComponent } from './cabinet/settings/profile/email-form';
-import { SettingsPasswordComponent } from './cabinet/settings/password';
-import { NoAccountComponent } from './cabinet/common/no-account';
-import { SmsCodeDialogComponent } from './common/sms-code-dialog/sms-code-dialog.component';
-import { ConfirmDialogComponent } from './common/confirm-dialog';
-import { AlertDialogComponent } from './common/alert-dialog/alert-dialog.component';
-import { LinkCounterComponent } from './common/link-counter/link-counter.component';
-import { AlertComponent } from './common/alert';
-import { NoContentComponent } from './no-content';
+//import { CabinetComponent } from './cabinet';
+//import { CabinetHeaderComponent } from './cabinet/header';
+//import { CabinetFooterComponent } from './cabinet/footer';
+//import { CabinetMainComponent } from './cabinet/main';
+//import { CabinetPaymentComponent } from './cabinet/payment';
+//import { CabinetTransferComponent } from './cabinet/transfer';
+//import { TransferDefaultComponent } from './cabinet/transfer/default';
+//import { TransferAccountComponent } from './cabinet/transfer/account';
+//import { CabinetFillComponent } from './cabinet/fill';
+//import { CabinetScoreComponent } from './cabinet/score';
+//import { ScoreIndexComponent } from './cabinet/score/default';
+//import { ScoreCreateComponent } from './cabinet/score/create';
+//import { CabinetOutmoneyComponent } from './cabinet/outmoney';
+//import { OutmoneyDefaultComponent } from './cabinet/outmoney/default';
+//import { OutmoneyListComponent } from './cabinet/outmoney/list';
+//import { CabinetSettingsComponent } from './cabinet/settings';
+//import { SettingsProfileComponent } from './cabinet/settings/profile';
+//import { PhoneFormComponent } from './cabinet/settings/profile/phone-form';
+//import { EmailFormComponent } from './cabinet/settings/profile/email-form';
+//import { SettingsPasswordComponent } from './cabinet/settings/password';
+//import { NoAccountComponent } from './cabinet/common/no-account';
+//import { SmsCodeDialogComponent } from './common/sms-code-dialog/sms-code-dialog.component';
+//import { ConfirmDialogComponent } from './common/confirm-dialog';
+//import { AlertDialogComponent } from './common/alert-dialog/alert-dialog.component';
+//import { LinkCounterComponent } from './common/link-counter/link-counter.component';
+//import { AlertComponent } from './common/alert';
+//import { NoContentComponent } from './no-content';
 
 // New template components
 import { AboutComponent } from './home-new/static/about/about.component';
@@ -118,33 +118,33 @@ type StoreType = {
     , SiteRegisterComponent
     , SiteConfirmComponent
     , SitePasswordComponent
-    , CabinetComponent
-    , CabinetHeaderComponent
-    , CabinetFooterComponent
-    , CabinetMainComponent
-    , CabinetPaymentComponent
-    , CabinetTransferComponent
-    , TransferDefaultComponent
-    , TransferAccountComponent
-    , CabinetFillComponent
-    , CabinetScoreComponent
-    , ScoreIndexComponent
-    , ScoreCreateComponent
-    , CabinetOutmoneyComponent
-    , OutmoneyDefaultComponent
-    , OutmoneyListComponent
-    , CabinetSettingsComponent
-    , SettingsProfileComponent
-    , PhoneFormComponent
-    , EmailFormComponent
-    , SettingsPasswordComponent
-    , SmsCodeDialogComponent
-    , ConfirmDialogComponent
-    , AlertDialogComponent
-    , LinkCounterComponent
-    , AlertComponent
-    , NoAccountComponent
-    , NoContentComponent
+    //, CabinetComponent
+    //, CabinetHeaderComponent
+    //, CabinetFooterComponent
+    //, CabinetMainComponent
+    //, CabinetPaymentComponent
+    //, CabinetTransferComponent
+    //, TransferDefaultComponent
+    //, TransferAccountComponent
+    //, CabinetFillComponent
+    //, CabinetScoreComponent
+    //, ScoreIndexComponent
+    //, ScoreCreateComponent
+    //, CabinetOutmoneyComponent
+    //, OutmoneyDefaultComponent
+    //, OutmoneyListComponent
+    //, CabinetSettingsComponent
+    //, SettingsProfileComponent
+    //, PhoneFormComponent
+    //, EmailFormComponent
+    //, SettingsPasswordComponent
+    //, SmsCodeDialogComponent
+    //, ConfirmDialogComponent
+    //, AlertDialogComponent
+    //, LinkCounterComponent
+    //, AlertComponent
+    //, NoAccountComponent
+    //, NoContentComponent
 
     // New template components
     , AboutComponent
