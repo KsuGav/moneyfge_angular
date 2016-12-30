d2ff1f91b8878340674610bcb2433d98f5fb6234
smhfanda <ikyrsthebest@gmail.com>
2016-12-30 16:42:18 +0200

    Added component

--- src/app/app.modules/cabinet_new.module.ts
+++ src/app/app.modules/cabinet_new.module.ts
@@ -1,11 +1,13 @@
 import { NewCabinetHeaderComponent } from '../cabinet-new/header/cabinet-header.component';
 import { DashboardComponent } from '../cabinet-new/dashboard/dashboard.component';
+import { SidebarComponent } from '../cabinet-new/dashboard/sidebar/sidebar.component';
 import { AccountComponent } from '../cabinet-new/dashboard/account/account.component';
 import { AccountsComponent } from '../cabinet-new/dashboard/accounts/accounts.component';
 
 export const CABINETNEW: any[]=[
   NewCabinetHeaderComponent
   , DashboardComponent
+  , SidebarComponent
   , AccountComponent
   , AccountsComponent
 ];
