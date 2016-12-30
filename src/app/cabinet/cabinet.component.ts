0ea8a0468d4ac749b1da1bb542689f9f5f94c887
smhfanda <ikyrsthebest@gmail.com>
2016-12-30 11:52:05 +0200

    Remove bootstrap from old cabinet main component.

--- src/app/cabinet/cabinet.component.ts
+++ src/app/cabinet/cabinet.component.ts
@@ -16,9 +16,6 @@ export class CabinetComponent implements OnInit, OnDestroy, AfterViewInit {
   constructor(private userService: User) {
     $('html').css('display', 'none');
 
-    // $('head').children('link#home').remove();
-
-    $('head').append('<link id="bootstrap" rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" />');
     $('head').append('<link id="styles1" rel="stylesheet" href="assets/css/cabinet/styles1.css" type="text/css" />');
     $('head').append('<link id="styles2" rel="stylesheet" href="assets/css/cabinet/styles2.css" type="text/css" />');
     $('head').append('<link id="main" rel="stylesheet" href="assets/css/cabinet/main.min.css" type="text/css" />');
@@ -36,9 +33,6 @@ export class CabinetComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnDestroy() {
-    // $('head').append('<link id="home" rel="stylesheet" href="assets/css/home.css" type="text/css" />');
-
-    $('head').children('link#bootstrap').remove();
     $('head').children('link#styles1').remove();
     $('head').children('link#styles2').remove();
     $('head').children('link#main').remove();
