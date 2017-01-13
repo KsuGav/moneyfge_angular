286afd76119b56b9720447e91d24bbe481ac1b4e
KsuGav <barabuga91>
2017-01-13 11:42:24 +0200

    Add settings page

--- src/app/cabinet-new/header/settings/settings.component.ts
+++ src/app/cabinet-new/header/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 declare const $: any;
 
@@ -7,7 +7,33 @@ declare const $: any;
     templateUrl: 'settings.component.html'
 })
 
-export class SettingsComponent  {
+export class SettingsComponent implements OnInit {
+
+    ngOnInit(){
+        this.settings();
+    }
+
+
+    settings(){
+        // Settings Page Functions
+        $('#SMSActivate').click(function () {
+            $('#SMSActive').slideDown();
+        });
+        $('#SMSDeActivate').click(function () {
+            $('#SMSActive').slideUp();
+        });
+
+        $('#WalletStep1').click(function () {
+            $('#WalletStep1Block').slideDown();
+        });
+        $('#WalletStep2').click(function () {
+            $('#WalletStep2Block').slideDown();
+        });
+        $('#WalletStep3').click(function () {
+            $('#WalletStep1Block, #WalletStep2Block').slideUp();
+            $('.wallet-block.success').fadeIn();
+        });
+    }
 
 }
 
