dfe4ba89529aa072a74aa8aef704a4989ca755b8
Andrii <aoromanchak@gmail.com>
2017-01-16 11:58:09 +0200

    Fix getHistory subscription

--- src/app/cabinet-new/dashboard/accounts/account-history.component.ts
+++ src/app/cabinet-new/dashboard/accounts/account-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild  } from '@angular/core';
+import {Component, ViewChild, OnDestroy} from '@angular/core';
 import { OnInit } from '@angular/core';
 import { n_AccountService } from '../../../app.services/Account.service';
 import { AccountHistoryRecord } from '../../../app.models/AccountHistoryRecord.model';
@@ -8,7 +8,7 @@ import { LoaderComponent } from "../../../common-new/loader/loader.component";
     selector: 'account-history-component',
     templateUrl: 'account-history.component.html'
 })
-export class AccountHistoryComponent implements OnInit {
+export class AccountHistoryComponent implements OnInit, OnDestroy {
 
     history: AccountHistoryRecord[] = [];
     noHistory: Boolean = false;
@@ -35,13 +35,16 @@ export class AccountHistoryComponent implements OnInit {
         this.setupElements();
     }
 
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+
     setupElements() {
 
     }
 
     getHistory() {
         this.historyLoader.toggle(true);
-        //this.accountService.getAccountHistory(this.accountService.initialAccountId);
     }
 
 }
