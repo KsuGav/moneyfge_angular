548df9f975dea1b1c64eecb0e3cc41811e69538f
KsuGav <barabuga91>
2017-01-18 11:45:53 +0200

    1. corrected functionality and html of buttons in security settings

--- src/app/cabinet-new/header/settings/security_settings/security-settings.component.ts
+++ src/app/cabinet-new/header/settings/security_settings/security-settings.component.ts
@@ -13,4 +13,33 @@ export class SecuritySettingsComponent implements OnInit {
     ngOnInit(){
 
     }
+
+    emailSubmit() {
+        if ($('#EmailLinkInput').val().length >= 5) {
+            $('#EmailLinkBLock').slideUp();
+        }
+    }
+
+    checkboxSmsOn(){
+        $('#SMSBLockBtn .on').css({'display':'block'});
+            $('#SMSBLockBtn .off').css({'display':'none'});
+
+    }
+    checkboxSmsOff(){
+        $('#SMSBLockBtn .on').css({'display':'none'});
+        $('#SMSBLockBtn .off').css({'display':'block'});
+    }
+
+    checkboxEmailOn(){
+        $('#EmailLinkBLockBtn .on').css({'display':'block'});
+        $('#EmailLinkBLockBtn .off').css({'display':'none'});
+
+    }
+    checkboxEmailOff(){
+        $('#EmailLinkBLockBtn .on').css({'display':'none'});
+        $('#EmailLinkBLockBtn .off').css({'display':'block'});
+        $('#EmailLinkBLock').slideDown();
+    }
+
+
 }
